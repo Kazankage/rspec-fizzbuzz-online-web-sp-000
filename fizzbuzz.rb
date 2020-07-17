cb585c894c1ea5489274a1a7dfb65fe0f89ff14c
Randel Jiao <randeljiao@gmail.com>
2020-07-17 22:48:50 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,10 +1,13 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 
+
 def fizzbuzz(number)
-  if number / 3 == % 0
-    return "Fizz"
-  elsif
-    number / 5 == % 0
-      return "Buzz"
+  if number % 3 == 0 && number % 5 == 0
+    "FizzBuzz"
+  elsif number % 3 == 0
+    "Fizz"
+  elsif number % 5 == 0
+    "Buzz"
+  end
 end
